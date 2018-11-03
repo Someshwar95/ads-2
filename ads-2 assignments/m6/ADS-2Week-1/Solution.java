bf2f68b7e9b180e6424273b3c90414179dd70c7c
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-11-03 11:10:57 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ads-2 assignments/m6/ADS-2Week-1/Solution.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class PageRank {
+	public PageRank(int graph) {
+
+	}
+	public double getPR(int v) {
+		return 0;
+	}
+	public String toString() {
+		return "";
+	}
+}
+
+class WebSearch {
+
+}
+
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int vertex = Integer.parseInt(sc.nextLine());
+		// read the first line of the input to get the number of vertices
+
+		// iterate count of vertices times 
+		// to read the adjacency list from std input
+		// and build the graph
+		
+		
+		// Create page rank object and pass the graph object to the constructor
+		
+		// print the page rank object
+		
+		// This part is only for the final test case
+		
+		// File path to the web content
+		String file = "WebContent.txt";
+		
+		// instantiate web search object
+		// and pass the page rank object and the file path to the constructor
+		
+		// read the search queries from std in
+		// remove the q= prefix and extract the search word
+		// pass the word to iAmFeelingLucky method of web search
+		// print the return value of iAmFeelingLucky
+		System.out.println("iAmFeelingLucky");
+		
+	}
+}

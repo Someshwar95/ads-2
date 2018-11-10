19e2b98df847f2546eb099e5f4931b13d7d86b2f
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-11-10 10:47:38 +0530

    "ads2_m12_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ads-2 assignments/m12/ADS-2_Exam-2/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Solution {
+
+	public static void main(String[] args) {
+		// Self loops are not allowed...
+		// Parallel Edges are allowed...
+		// Take the Graph input here...
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < k; j++ ) {
+				
+			}
+			
+		}
+
+		String caseToGo = null;
+		switch (caseToGo) {
+		case "Graph":
+			//Print the Graph Object.
+			break;
+
+		case "DirectedPaths":
+			// Handle the case of DirectedPaths, where two integers are given.
+			// First is the source and second is the destination.
+			// If the path exists print the distance between them.
+			// Other wise print "No Path Found."
+			break;
+
+		case "ViaPaths":
+			// Handle the case of ViaPaths, where three integers are given.
+			// First is the source and second is the via is the one where path should pass throuh.
+			// third is the destination.
+			// If the path exists print the distance between them.
+			// Other wise print "No Path Found."
+			break;
+
+		default:
+			break;
+		}
+
+	}
+}

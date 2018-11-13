de588ca39dd8cf002b78dd15257398d04bf8adfa
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-11-13 15:19:35 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ads-2 assignments/m13/Radix Sorts/LSD.java
+++ ads-2 assignments/m13/Radix Sorts/LSD.java
@@ -18,20 +18,20 @@ public class LSD {
      */
     public static void sort(final String[] a, final int w) {
         int n = a.length;
-        int R = 256;   // extend ASCII alphabet size
+        int rR = 256;   // extend ASCII alphabet size
         String[] aux = new String[n];
 
         for (int d = w - 1; d >= 0; d--) {
             // sort by key-indexed counting on dth character
 
             // compute frequency counts
-            int[] count = new int[R + 1];
+            int[] count = new int[rR + 1];
             for (int i = 0; i < n; i++) {
                 count[a[i].charAt(d) + 1]++;
             }
 
             // compute cumulates
-            for (int r = 0; r < R; r++) {
+            for (int r = 0; r < rR; r++) {
                 count[r + 1] += count[r];
             }
 
@@ -55,8 +55,8 @@ public class LSD {
      */
     public static void sort(final int[] a) {
         final int bits = 32;                 // each int is 32 bits
-        final int R = 1 << BITS_PER_BYTE;    // each bytes is between 0 and 255
-        final int mask = R - 1;              // 0xFF
+        final int rR = 1 << BITS_PER_BYTE;    // each bytes is between 0 and 255
+        final int mask = rR - 1;              // 0xFF
         final int w = bits / BITS_PER_BYTE;  // each int is 4 bytes
 
         int n = a.length;
@@ -65,25 +65,25 @@ public class LSD {
         for (int d = 0; d < w; d++) {
 
             // compute frequency counts
-            int[] count = new int[R + 1];
+            int[] count = new int[rR + 1];
             for (int i = 0; i < n; i++) {
                 int c = (a[i] >> BITS_PER_BYTE * d) & mask;
                 count[c + 1]++;
             }
 
             // compute cumulates
-            for (int r = 0; r < R; r++) {
+            for (int r = 0; r < rR; r++) {
                 count[r + 1] += count[r];
             }
 
             // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
             if (d == w - 1) {
-                int shift1 = count[R] - count[R / 2];
-                int shift2 = count[R / 2];
-                for (int r = 0; r < R / 2; r++) {
+                int shift1 = count[rR] - count[rR / 2];
+                int shift2 = count[rR / 2];
+                for (int r = 0; r < rR / 2; r++) {
                     count[r] += shift1;
                 }
-                for (int r = R / 2; r < R; r++) {
+                for (int r = rR / 2; r < rR; r++) {
                     count[r] -= shift2;
                 }
             }

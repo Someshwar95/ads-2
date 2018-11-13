cf4331c9b73fd129ddf0c289b2f881ba42609595
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-11-13 14:44:53 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ads-2 assignments/m13/Radix Sorts/LSD.java
+++ ads-2 assignments/m13/Radix Sorts/LSD.java
@@ -62,18 +62,19 @@ public class LSD {
         int n = a.length;
         int[] aux = new int[n];
 
-        for (int d = 0; d < w; d++) {         
+        for (int d = 0; d < w; d++) {
 
             // compute frequency counts
             int[] count = new int[R + 1];
-            for (int i = 0; i < n; i++) {           
+            for (int i = 0; i < n; i++) {
                 int c = (a[i] >> BITS_PER_BYTE * d) & MASK;
                 count[c + 1]++;
             }
 
             // compute cumulates
-            for (int r = 0; r < R; r++)
+            for (int r = 0; r < R; r++) {
                 count[r + 1] += count[r];
+            }
 
             // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
             if (d == w - 1) {
@@ -124,3 +125,5 @@ public class LSD {
     //         StdOut.println(a[i]);
     // }
 }
+
+

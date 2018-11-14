1cd2ce3b4c0c8f2558c4077512e9d4153bdaf189
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-11-14 15:45:47 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ads-2 assignments/m14/Assignment-1/Solution.java
+++ ads-2 assignments/m14/Assignment-1/Solution.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
  * Class for solution.
  */
 final class Solution {
-	/**.
-	 * Constructs the object.
-	 */
-	private Solution() {
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		String[] words = loadWords();
-		//Your code goes here...
-		TST<Integer> tree = new TST<Integer>();
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        String[] words = loadWords();
+        //Your code goes here...
+        TST<Integer> tree = new TST<Integer>();
         for (int i = 0; i < words.length; i++) {
             String[] suffix = new String[words[i].length()];
             for (int j = 0; j < words[i].length(); j++) {
@@ -31,15 +31,16 @@ final class Solution {
         for (String i: tree.keysWithPrefix(text)) {
             System.out.println(i);
         }
-	}
-	/**.
-	 * Loads words.
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static String[] loadWords() {
-		In in = new In("/Files/dictionary-algs4.txt");
-		String[] words = in.readAllStrings();
-		return words;
-	}
+    }
+    /**.
+     * Loads words.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String[] loadWords() {
+        In in = new In("/Files/dictionary-algs4.txt");
+        String[] words = in.readAllStrings();
+        return words;
+    }
 }
+
--- ads-2 assignments/m14/Assignment-1/TST.java
+++ ads-2 assignments/m14/Assignment-1/TST.java
@@ -293,3 +293,5 @@ public class TST<Value> {
         }
     }
 }
+
+

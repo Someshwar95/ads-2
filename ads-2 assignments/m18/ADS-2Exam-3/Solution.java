53626d518178899f4c46896267d4f2eee6e0b025
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-11-17 10:53:30 +0530

    "ads2_m18_a1_1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads-2 assignments/m18/ADS-2Exam-3/Solution.java
+++ ads-2 assignments/m18/ADS-2Exam-3/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
 		switch (cases) {
 		case "loadDictionary": 
 			// input000.txt and output000.txt
-			Map dictionary = new HashMap();
+			Dictionary d = new Hashtable();
 			BinarySearchST<String, Integer> hash = loadDictionary("/Files/t9.csv");
 			while (scan.hasNextLine()) {
 				String key = scan.nextLine();
